5d78c4d2c56d370712d2fdef58323f7c38865d1e
Quentin Monnet <quentin.monnet@netronome.com>
2018-11-09 13:03:30 +0000

    BACKPORT: bpf: pass destroy() as a callback and remove its ndo_bpf subcommand
    As part of the transition from ndo_bpf() to callbacks attached to struct
    bpf_offload_dev for some of the eBPF offload operations, move the
    functions related to program destruction to the struct and remove the
    subcommand that was used to call them through the NDO.
    
    Remove function __bpf_offload_ndo(), which is no longer used.
    
    Change-Id: Ibd8de86bf01cd52322205cf64c0fe4a94c1c716c
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: z3rokwq <z3rokwq@gmail.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -805,7 +805,6 @@ enum bpf_netdev_command {
 	XDP_SETUP_PROG_HW,
 	XDP_QUERY_PROG,
 	/* BPF program for offload callbacks, invoked at program load time. */
-	BPF_OFFLOAD_DESTROY,
 	BPF_OFFLOAD_MAP_ALLOC,
 	BPF_OFFLOAD_MAP_FREE,
 };
@@ -829,10 +828,6 @@ struct netdev_bpf {
 			/* flags with which program was installed */
 			u32 prog_flags;
 		};
-		/* BPF_OFFLOAD_DESTROY */
-		struct {
-			struct bpf_prog *prog;
-		} offload;
 		/* BPF_OFFLOAD_MAP_ALLOC, BPF_OFFLOAD_MAP_FREE */
 		struct {
 			struct bpf_offloaded_map *offmap;
