f8bcca9e92fa666aedd289de71df7cc41c640e06
Eric Dumazet <edumazet@google.com>
2024-05-28 11:43:53 +0000

    BACKPORT: net: fix __dst_negative_advice() race
    __dst_negative_advice() does not enforce proper RCU rules when
    sk->dst_cache must be cleared, leading to possible UAF.
    
    RCU rules are that we must first clear sk->sk_dst_cache,
    then call dst_release(old_dst).
    
    Note that sk_dst_reset(sk) is implementing this protocol correctly,
    while __dst_negative_advice() uses the wrong order.
    
    Given that ip6_negative_advice() has special logic
    against RTF_CACHE, this means each of the three ->negative_advice()
    existing methods must perform the sk_dst_reset() themselves.
    
    Note the check against NULL dst is centralized in
    __dst_negative_advice(), there is no need to duplicate
    it in various callbacks.
    
    Many thanks to Clement Lecigne for tracking this issue.
    
    This old bug became visible after the blamed commit, using UDP sockets.
    
    Bug: 343727534
    Fixes: a87cb3e48ee8 ("net: Facility to report route quality of connected sockets")
    Reported-by: Clement Lecigne <clecigne@google.com>
    Diagnosed-by: Clement Lecigne <clecigne@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Tom Herbert <tom@herbertland.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Link: https://lore.kernel.org/r/20240528114353.1794151-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [Lee: Trivial/unrelated conflict - no change to the patch]
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I293734dca1b81fcb712e1de294f51e96a405f7e4
    Signed-off-by: z3rokwq <z3rokwq@gmail.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -2234,6 +2234,7 @@ static void ip6_negative_advice(struct sock *sk,
 	struct rt6_info *rt = (struct rt6_info *) dst;
 
 	if (rt->rt6i_flags & RTF_CACHE) {
+		rcu_read_lock();
 		if (rt6_check_expired(rt)) {
 			/* counteract the dst_release() in sk_dst_reset() */
 			dst_hold(dst);
@@ -2241,6 +2242,7 @@ static void ip6_negative_advice(struct sock *sk,
 
 			rt6_remove_exception_rt(rt);
 		}
+		rcu_read_unlock();
 		return;
 	}
 	sk_dst_reset(sk);
