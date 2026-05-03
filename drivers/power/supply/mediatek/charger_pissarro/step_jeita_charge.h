3bbc66b36e9699f5f74bfc9847509a4911922c4b
z3rokwq <z3rokwq@gmail.com>
2026-05-03 10:33:26 +0300

    drivers: power: supply: mediatek: charger_pissarro: Refactor cycle count status logic
    * Based on disassembly stock kernel from OS1.0.18.0.TKTMIXM
    
    Signed-off-by: z3rokwq <z3rokwq@gmail.com>

--- drivers/power/supply/mediatek/charger_pissarro/step_jeita_charge.h
+++ drivers/power/supply/mediatek/charger_pissarro/step_jeita_charge.h
@@ -1,3 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * step/jeita charge controller
+ *
+ * published by the Free Software Foundation.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*	date			author			comment
+ *	2021-06-01		chenyichun@xiaomi.com	create
+ */
+
+#ifndef __STEP_JEITA_CHARGE_H
+#define __STEP_JEITA_CHARGE_H
+
 #define STEP_JEITA_TUPLE_COUNT	6
 #define THERMAL_LIMIT_COUNT	16
 #define THERMAL_LIMIT_TUPLE	6
@@ -21,16 +39,19 @@ struct step_jeita_cfg0 {
 struct step_jeita_cfg1 {
 	int low_threshold;
 	int high_threshold;
-        int extra_threshold;
+	int extra_threshold;
 	int low_value;
-        int high_value;
+	int high_value;
 };
 
 enum cycle_count_status {
-	CYCLE_COUNT_LOW,
-	CYCLE_COUNT_NORMAL,
-	CYCLE_COUNT_HIGH,
+	CYCLE_COUNT_0_100,
+	CYCLE_COUNT_101_600,
+	CYCLE_COUNT_601_800,
+	CYCLE_COUNT_801_9999,
 };
 
 void reset_mi_charge_alg(struct charger_manager *info);
 int step_jeita_init(struct charger_manager *info, struct device *dev, int product_name);
+
+#endif /* __STEP_JEITA_CHARGE_H */
